e2814f0be6cccbc6eb3f6bccd36f2a8e26b8f4c3
Daniel Feustel <feustel.daniel03@gmail.com>
2022-07-15 23:07:16 +0200

    init commit

---
+++ index.js
@@ -0,0 +1,91 @@
+
+
+
+let text = document.getElementById("notepad");
+let header = document.getElementById("header");
+let save = document.getElementById("save").addEventListener("click", getText);
+let clear = document.getElementById("clear").addEventListener("click",clearNote)
+let del = document.getElementById("delete").addEventListener("click",deleteNote)
+
+
+let listArray = [];
+let noteArray = new Map();
+let counter = 0;
+
+
+function deleteNote(){
+
+  let top = header.value;
+
+  noteArray.delete(top);
+  console.log(noteArray);
+   
+}
+
+function clearNote(){
+  header.value = "";
+  text.value = "";
+}
+function getText()
+{
+
+  let note = text.value;
+  let topic= header.value;
+  
+  if(note == '' || topic == '')
+  {
+    alert("Missing Statement")
+    return
+  }
+
+
+  if(noteArray.has(topic)){
+
+    alert("Already Defined")
+    return;
+  }
+
+  noteArray.set(topic,note);
+  console.log(noteArray);
+  
+
+  text.value = '';
+  header.value = '';
+
+  
+  addListElement();
+
+  function addListElement()
+  {
+
+    let list = document.getElementById("list");
+    let output = topic
+
+    
+    
+    
+    list.innerHTML += "<li class= liste  id=liste>" + output +"</li>";
+    
+    let a = document.getElementById("list").getElementsByTagName('li');
+    for(let i=0;i<a.length;i++){
+      a[i].addEventListener("click",function(){load(this.innerHTML)})
+    }
+    //console.log(a)    
+  }
+  counter++;
+}
+
+
+
+function load(d){
+  
+  console.log(d);
+  let head = d;
+  let entry = noteArray.get(d);
+
+  header.value = head;
+  text.value = entry;
+
+  console.log(listArray)
+}
+
